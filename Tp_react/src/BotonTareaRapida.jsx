eb211b773394b610d925d41b4abc67bddf26c328
AGUSTIN ZAID ALESSI <48385003@ort>
2025-04-22 09:05:11 -0300

    avances

---
+++ Tp_react/src/BotonTareaRapida.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BotonTareaRapida({tarearapida}) {
+  return 
+  (
+    <button onclick="TareaMasRapida()">Mostrar tarea más rápida</button>
+  )
+}
+export default BotonTareaRapida;
